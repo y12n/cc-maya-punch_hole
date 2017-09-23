9dfb68f0d428f7e6cf858b08667c006b7e812c7f
kidintwo3 <hunyadijanos@Hunyadi-MacBook-Air.local>
2017-09-23 14:57:00 +0200

    mac builds

--- make/build_osx.sh
+++ make/build_osx.sh
@@ -1,4 +1,4 @@
-versions=('2015' '2016' '2016.5' '2017')
+versions=('2016' '2016.5' '2017' '2018')
 op_system='mac'
 plugin_name='puncher'
 make_dir='/Users/hunyadijanos/Documents/GIT/cc-maya-punch_hole/make/build/'$op_system'/'
